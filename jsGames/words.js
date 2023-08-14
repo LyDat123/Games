b03b99fc5548f9fbe0b792f6b0e9a6fc83787c68
LyDat123 <lydat12a1@gmail.com>
2023-08-14 17:39:29 +0700

    Games

---
+++ jsGames/words.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "Cat",
+        hint: "A furry animal that purrs and likes to chase mice"
+    },
+    {
+        word: "Dog",
+        hint: " A loyal pet that barks and often plays fetch."
+    },
+    {
+        word: "Toy",
+        hint: "Something kids play with, like action figures or dolls."
+    },
+    {
+        word: "Fish",
+        hint: " Swims in the water and often has scales"
+    },
+    {
+        word: "Bubble",
+        hint: "What you blow with soap and water, and it floats in the air."
+    },
+    {
+        word: "Mom",
+        hint: "The person who takes care of you and loves you a lot"
+    },
+    {
+        word: "Doll",
+        hint: "A soft toy that often looks like a little person."
+    },
+    {
+        word: "Car",
+        hint: " Takes you on trips and has wheels"
+    },
+    {
+        word: "School",
+        hint: "A place where you learn and make friends"
+    },
+    {
+        word: "Flower",
+        hint: "A beautiful plant with colorful petals that grows in gardens"
+    },
+    {
+        word: "Book",
+        hint: "Something you read, with words and pictures."
+    },
+    {
+        word: "Bird",
+        hint: "Flies in the sky and sings a beautiful song."
+    },
+    {
+        word: "Family",
+        hint: "The people who love you and live with you."
+    },
+    {
+        word: "Teddy bear",
+        hint: "A cuddly stuffed animal that's often your bedtime friend."
+    },
+    {
+        word: "Zoo",
+        hint: "A place where you can see many different animals from all over the world."
+    },
+    {
+        word: "Moon",
+        hint: " It lights up the night sky and looks like a big circle"
+    },
+    {
+        word: "Friend",
+        hint: "Person other than a family member"
+    },
+    {
+        word: "Ice cream",
+        hint: "A frozen treat that comes in many flavors, great for hot days."
+    },
+    {
+        word: "Lion",
+        hint: "A powerful animal, often called the king of the jungle."
+    },
+    {
+        word: "Cake",
+        hint: "A delicious dessert often eaten at celebrations like birthdays."
+    },
+    {
+        word: "Tiger",
+        hint: "A big cat with orange fur and black stripes."
+    },
+    {
+        word: "Rainbow",
+        hint: " A colorful arc in the sky after rain, with red, orange, yellow, and more."
+    },
+    {
+        word: "Library",
+        hint: "Place containing collection of books"
+    },
+]
